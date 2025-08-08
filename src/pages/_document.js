92f8d3d75408481840bb9e3c3910f15f48362721
developerfabiocsilva@outlook.com <developerfabiocsilva@outlook.com>
2025-08-08 19:24:18 -0300

    inserindo icon _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icon-192.png" />
         <meta name="theme-color" content="#007bff" />
       </Head>
       <body>
